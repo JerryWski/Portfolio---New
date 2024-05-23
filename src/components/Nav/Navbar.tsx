240774c89e8365511c56963d1dc711381edbeb0c
JerryWski <jarek_wasilewski@yahoo.com>
2024-05-23 22:24:04 +0200

    styling navbar

--- src/components/Nav/Navbar.tsx
+++ src/components/Nav/Navbar.tsx
@@ -2,11 +2,14 @@ import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <ul className='nav-list'>
-        <li>About</li>
-        <li>Bio</li>
-        <li>Projekty</li>
-    </ul>
+    <div className='nav-container'>
+      <ul className='nav-list'>
+          <li className='nav-list_item'>_o mnie</li>
+          <li className='nav-list_item'>_bio</li>
+          <li className='nav-list_item'>_projekty</li>
+          <li className='nav-list_item'>_kontakt</li>
+      </ul>
+    </div>
   )
 }
 
