efc84cc704898243d538e2371b95d61b46bf04e2
JerryWski <jarek_wasilewski@yahoo.com>
2024-05-23 22:01:55 +0200

    setting up things

---
+++ src/components/Nav/Navbar.tsx
@@ -0,0 +1,13 @@
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <ul className='nav-list'>
+        <li>About</li>
+        <li>Bio</li>
+        <li>Projekty</li>
+    </ul>
+  )
+}
+
+export default Navbar;
