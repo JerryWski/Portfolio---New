3bd588dddad60777c004164a2d4c1b30f25640c9
JerryWski <jarek_wasilewski@yahoo.com>
2024-06-11 21:21:19 +0200

    installing lottie

---
+++ src/utils/LottieIcon.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Lottie from "lottie-react";
+import github from "../icons/github.json";
+
+const LottieIcon = () => <Lottie animationData={github} loop={true} />;
+
+export default LottieIcon;
