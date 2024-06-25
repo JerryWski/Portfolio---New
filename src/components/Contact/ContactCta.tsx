fbaf92337c721e74d43a28565ad3d1e1f601ae4e
JerryWski <jarek_wasilewski@yahoo.com>
2024-06-25 22:03:49 +0200

    polishing contact section

--- src/components/Contact/ContactCta.tsx
+++ src/components/Contact/ContactCta.tsx
@@ -1,12 +1,12 @@
-import { motion, useInView  } from 'framer-motion';
+import { motion, useInView } from 'framer-motion';
 import { useState, useRef, useEffect } from 'react';
 import './ContactCta.css';
 
 const ContactCta = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [triggered, setTriggered] = useState(false);
-  const ref = useRef(null)
-  const isInView = useInView(ref, { margin: '-200px 0px' })
+  const ref = useRef(null);
+  const isInView = useInView(ref, { margin: '-200px 0px' });
 
   useEffect(() => {
     if (isInView) {
@@ -30,27 +30,35 @@ const ContactCta = () => {
   return (
     <>
       <div ref={ref} className='container'>
-        <ul className='link-list'>
-          {links.map((link, index) => (
-            <motion.li
-              key={link.href}
-              initial={{ opacity: 0, x: 20 }}
-              animate={{ opacity: isOpen ? 1 : 0, x: isOpen ? 0 : 20 }}
-              transition={{ delay: isOpen ? 0.1 * index : 0, duration: 0.3 }}
-            >
-              <a href={link.href}>{link.text}</a>
-            </motion.li>
-          ))}
-        </ul>
+        <motion.div
+          initial={{ opacity: 0, x: 20 }}
+          animate={{ opacity: isOpen ? 1 : 0, x: isOpen ? 0 : 20 }}
+          transition={{ duration: 0.3 }}
+          className='lists-block'
+        >
+          <span className='link-sh'>Shortcuts</span>
+          <ul className='link-list'>
+            {links.map((link, index) => (
+              <motion.li
+                key={link.href}
+                transition={{ delay: isOpen ? 0.1 * index : 0, duration: 0.3 }}
+                initial={{ opacity: 0, x: 20 }}
+                animate={{ opacity: isOpen ? 1 : 0, x: isOpen ? 0 : 20 }}
+              >
+                <a href={link.href}>{link.text}</a>
+              </motion.li>
+            ))}
+          </ul>
+        </motion.div>
         <div className='word'>
           Let's W
           <button className='letter' onClick={handleToggle}>
-          <motion.span 
-            className='custom-radio'
-            animate={{ x: isOpen ? -20 : 0 }}
-            transition={{ duration: 0.3 }}
-          ></motion.span>
-        </button>
+            <motion.span
+              className='custom-radio'
+              animate={{ x: isOpen ? -20 : 0 }}
+              transition={{ duration: 0.3 }}
+            ></motion.span>
+          </button>
           rk Together
         </div>
         <motion.div
